f5924e14b6c079dc7fc671e0e2564df9ac925b32
maxmellon <maxmellon@v157-7-131-230.myvps.jp>
2015-05-17 18:18:40 +0900

    edit: install script内にhelpのclone記述

--- neo_bundle_install.sh
+++ neo_bundle_install.sh
@@ -1,4 +1,7 @@
+#/bin/sh
 mkdir -p ~/.vim/bundle
 mkdir -p ~/.vim/_tmp
 cd
 git clone git://github.com/Shougo/neobundle.vim .vim/bundle/neobundle.vim
+cd ~/.vim/help/vimdoc-ja
+git clone https://github.com/vim-jp/vimdoc-ja.git
