1aa7d99a6e2524cb3ebc74810ef1ae1ebaac0558
maxmellon <mozi_kke@excite.co.jp>
2015-05-21 08:26:53 +0900

    (script) fix: typo

--- neo_bundle_install.sh
+++ neo_bundle_install.sh
@@ -4,4 +4,4 @@ ln -s ./vim/.vimrc
 git clone git://github.com/Shougo/neobundle.vim .vim/bundle/neobundle.vim
 cd ~/.vim/help
 git clone https://github.com/vim-jp/vimdoc-ja.git
-neo +":NeoBundleInstall"+:q
+vim +":NeoBundleInstall"+:q
