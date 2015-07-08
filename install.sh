fa4651098cca8d0fba131c8a09f1c7b00a0c2b9c
MaxMEllon <mozi_kke@excite.co.jp>
2015-07-08 12:02:10 +0900

    install時のoption追加

--- install.sh
+++ install.sh
@@ -2,4 +2,12 @@
 ln -s ~/.vim/.vimrc ~/
 ln -s ~/.vim/.vimshrc ~/
 git submodule update --init
-vim +":NeoBundleInstall"+:q
+if [ $# -eq 0 ]; then
+  git checkout master
+  vim +":NeoBundleInstall"+:q
+elif [ $1 = "minimal" ]; then
+  git checkout minimal-mode
+  vim +":NeoBundleInstall"+:q
+else
+  echo "no such option ${1}"
+fi
