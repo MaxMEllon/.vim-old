ae7dd1b9765c0b1fc5e92cb8a00686a0bb0a590d
MaxMEllon <mozi_kke@excite.co.jp>
2015-07-08 13:12:27 +0900

    update install script

--- install.sh
+++ install.sh
@@ -4,10 +4,9 @@ ln -s ~/.vim/.vimshrc ~/
 git submodule update --init
 if [ $# -eq 0 ]; then
   git checkout master
-  vim +":NeoBundleInstall"+:q
 elif [ $1 = "minimal" ]; then
   git checkout minimal-mode
-  vim +":NeoBundleInstall"+:q
 else
   echo "no such option ${1}"
 fi
+vim +":NeoBundleInstall"+:q
