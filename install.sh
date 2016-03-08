58af310947a7e06cf7fa1cff923d5c43c4240452
MaxMEllon <mozi_kke@excite.co.jp>
2016-03-08 23:03:24 +0900

    Update install script

--- install.sh
+++ install.sh
@@ -1,5 +1,21 @@
 #/bin/sh
-ln -s ~/.vim/.vimrc ~/
-ln -s ~/.vim/.vimshrc ~/
+
+set -eu
+
+DOTPATH=~/.dotfiles
+
+if [ -n "$1" ]; then
+  DOTPATH=$1
+fi
+
+ln -s $1/vim ~/.vim &> /dev/null
+ln -s ~/.vim/.vimrc ~/ &> /dev/null
+ln -s ~/.vim/.vimshrc ~/ &> /dev/null
+mkdir -p ~/.config/nvim
+touch ~/.config/nvim/init.vim
+cat > ~/.config/nvim/init.vim <<EOF
+source ~/.vim/.nvimrc
+source ~/.vim/.vimrc
+EOF
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > /dev/null
